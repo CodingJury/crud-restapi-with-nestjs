5a7ae21090db1db6b4a4b9efdf204db67cdfe498
Dhananjay Patel <dhananjaypatel.mailto@gmail.com>
2023-11-11 20:18:34 +0530

    basic auth endpoint added

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common"
+
+@Injectable()
+export class AuthService {
+  signup() {
+    return {msg: 'Signup'}
+  }
+
+  signin() {
+    return {msg: 'Signin'}
+  }
+
+}
