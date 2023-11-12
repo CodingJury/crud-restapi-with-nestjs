870c90775cc0c1a55315de6cdda9c7cf1455fb9f
Dhananjay Patel <dhananjaypatel.mailto@gmail.com>
2023-11-12 11:31:57 +0530

    signup and signin logic added | prisma schema modified | script to run database added in 'package.json'

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,32 +1,59 @@
-import { Injectable } from "@nestjs/common"
+import { ForbiddenException, Injectable } from "@nestjs/common"
 import { PrismaService } from "src/prisma/prisma.service"
 import { AuthDto } from "./dto"
 import * as argon from "argon2"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async signup(dto: AuthDto) {
     //1. generate the passoword hash
     const hash = await argon.hash(dto.password)
-    
+
     //2 .save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash
+    try {
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        }
+      })
+
+      //3.return the saved user
+      delete user.hash //! little hack to prevent hash from exposed
+
+      return user
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new ForbiddenException('Credentials taken')
+        }
       }
-    })
-
-    //3.return the saved user
-    delete user.hash // little hack to prevent hash from exposed
-
-    return user
+      throw error;
+    }
   }
 
-  signin() {
-    return {msg: 'Signin'}
+  async signin(dto: AuthDto) {
+    //1. find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    })
+    //if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect')
+    
+    //2. compare password
+    const pwMatches = await argon.verify(user.hash, dto.password)
+    //if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect')
+
+    //send back the user
+    delete user.hash //!  little hack to prevent hash from exposed
+    return user
   }
 
 }
