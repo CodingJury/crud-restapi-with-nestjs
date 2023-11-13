07d29de3f74dfb6daeab05f311dedde50bd73710
Dhananjay Patel <dhananjaypatel.mailto@gmail.com>
2023-11-13 09:39:41 +0530

    modified sleep function to accept time in ms

--- sleep.js
+++ sleep.js
@@ -5,10 +5,13 @@ function sleep(ms) {
 }
 
 async function main() {
-  const args = process.argv.slice(2); //! [0:node, 1:sleep.js, 2: 3000]  we want the time in ms
-  const sleepDuration = args[0] || 5000; //Default to 5000ms if no argument is provided
+  const args = process.argv.slice(2); //! [0:node, 1:sleep.js, 2: 3000ms]  we want the time in ms
+  let sleepDuration = 5000; //Default to 5000ms if no argument is provided
+  if(args[0].endsWith("ms")) {
+    sleepDuration = parseInt(args[0],10); //Base 10 number system
+  }
 
-  console.log(`--->Going to sleep for ${sleepDuration/1000} seconds`)
+  console.log(`--->Going to sleep for ${sleepDuration} milliseconds`)
   await sleep(sleepDuration);
   console.log(`--->I woke up`)
 }
